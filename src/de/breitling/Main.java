c3be2bc12371f3b840cbe18db3861830fe72286b
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-05-26 08:03:13 -0400

    Add perent data logic.

--- src/de/breitling/Main.java
+++ src/de/breitling/Main.java
@@ -1,7 +1,22 @@
 package de.breitling;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Ask for Data from Data from parent
+        System.out.println("Hallo Vater oder Mutter. Bitte gib deinen Daten ein.");
         System.out.println("Guten Tag, Gib dein Name ein.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Gib dein Alter ein.");
+        String age = scanner.nextLine();
+
+        System.out.println("Gib den Namen deiners Sohes oder deiner Tocher ein.");
+        String childName = scanner.nextLine();
+
+
+
     }
 }
